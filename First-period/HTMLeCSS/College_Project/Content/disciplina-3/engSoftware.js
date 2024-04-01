9d51125e39e18302651022663c1a0c3d59126b58
Emanuelprime <emanuelll3000@gmail.com>
2024-04-01 19:18:02 -0300

    att

--- First-period/HTMLeCSS/College_Project/Content/disciplina-3/engSoftware.js
+++ First-period/HTMLeCSS/College_Project/Content/disciplina-3/engSoftware.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Selecionando todos os elementos com a classe "subject-folder"
+    /* Selecionando todos os elementos com a classe "subject-folder"*/
     const subjectFolders = document.querySelectorAll('.subject-folder');
 
-    // Iterando sobre cada elemento com a classe "subject-folder"
+    /* Iterando sobre cada elemento com a classe "subject-folder"*/
     subjectFolders.forEach(folder => {
-        // Adicionando um evento de clique ao elemento de controle (button.toggle)
+        /* Adicionando um evento de clique ao elemento de controle (button.toggle)*/
         folder.querySelector('.toggle').addEventListener('click', function () {
-            // Selecionando o conteúdo dentro do folder
+            /* Selecionando o conteúdo dentro do folder*/
             const content = folder.querySelector('.content');
-            // Alternando a classe "expanded" no elemento pai ".subject-folder"
+            /* Alternando a classe "expanded" no elemento pai ".subject-folder"*/
             folder.classList.toggle('expanded');
-            // Alternando a classe "show" para controlar a visibilidade do conteúdo
+            /* Alternando a classe "show" para controlar a visibilidade do conteúdo*/
             content.classList.toggle('show');
         });
     });
