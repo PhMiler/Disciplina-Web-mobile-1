222c3ed0d07e6997629ea635e9d1a325ad6489e2
Paulo <engs-paulomariano@camporeal.edu.br>
2024-04-26 18:22:54 -0300

    adicionando novos arquivos

---
+++ First-period/JavaScript/javascript/codigo.js
@@ -0,0 +1,44 @@
+let nome = prompt("Qual é o seu nome?");
+console.log("Olá," + nome + "!");
+
+
+// 1) Escreva um programa em JavaScript que exiba na tela o maior número
+// entre dois números fornecidos pelo usuário.
+
+let num1 = +prompt("Digete um número:");
+let num2 = +prompt("Digite mais um número:")
+if(num1 > num2){
+    console.log('O número maior é:', num1);  
+}else{
+    console.log('O número maior é:', num2);
+}
+
+// 2) Escreva um programa em JavaScript que exiba na tela o maior número
+// entre dois números fornecidos pelo usuário.
+
+
+// 3) Escreva um programa em JavaScript que exiba na tela uma mensagem
+// de acordo com a nota do usuário. Se a nota for menor do que 6, exiba
+// "Reprovado". Se a nota for maior ou igual a 6 e menor do que 8, exiba
+// "Aprovado". Se a nota for maior ou igual a 8, exiba "Aprovado com louvor".
+// 4) Escreva um programa em JavaScript que exiba na tela a média dos
+// números de um vetor.
+// 5) Escreva um programa em JavaScript que exiba na tela uma mensagem
+// de acordo com o valor de uma variável. Se a variável for igual a "a", exiba
+// "A". Se a variável for igual a "b", exiba "B". Se a variável não for nem "a"
+// nem "b", exiba "Outra letra".
+// 6) Escreva um programa em JavaScript que exiba na tela a soma dos
+// elementos de uma matriz.
+// 7) Escreva um programa em JavaScript que exiba na tela o fatorial de um
+// número fornecido pelo usuário.
+// 8) Escreva um programa em JavaScript que exiba na tela uma mensagem
+// de acordo com a escolha do usuário em um menu de opções. O menu
+// deve ter as opções "1. Opção 1", "2. Opção 2" e "3. Opção 3". Se o usuário
+// escolher a opção 1, exiba "Opção 1 escolhida". Se o usuário escolher a
+// opção 2, exiba "Opção 2 escolhida". Se o usuário escolher a opção 3,
+// exiba "Opção 3 escolhida". Se o usuário escolher uma opção inválida,
+// exiba "Opção inválida".
+// 9) Escreva um programa em JavaScript que exiba na tela o maior número
+// de um vetor.
+// 10)Escreva um programa em JavaScript que exiba na tela a média dos
+// elementos de uma matriz.
