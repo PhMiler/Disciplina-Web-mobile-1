8ac2d8e0eab8a2fdf5232564991df286097c66df
PhMiler <engs-paulomariano@camporeal.edu.br>
2024-05-27 22:08:00 -0300

    Continuação aula hooks

--- First-period/hooks/todolist/src/App.js
+++ First-period/hooks/todolist/src/App.js
@@ -1,8 +1,16 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect, useMemo} from 'react';
 
 function App(){
   const [tarefas, setTarefas] = useState([]);
   const [input, setInput] = useState("");
+  const [numero, setNumero] = useState(0);
+  const calculo = useMemo(()=>{
+    let resultado = 0;
+    for(let i = 0; i < numero * 1000; i++){
+      resultado += i;
+    }
+    return resultado;
+  }, [numero]);
   
   useEffect(() => {
     
@@ -31,7 +39,13 @@ return(
     </ul>
     <input type="text" value={input} onChange={paulo => setInput(paulo.target.value)}/>
     <button type="button" onClick={adicionarTarefa}>Adicionar</button>
+    <div>
+      <p>Número: {numero}</p>
+      <p>Resultado do cálculo: {calculo}</p>
+      <button onClick={() => setNumero(numero+1)}>Incrementer</button>
+    </div>
   </div>
+  
 )
 }
 export default App;
